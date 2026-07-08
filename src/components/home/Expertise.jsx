7e970171267d08f51b301db5c94d81d95dc28249
sunny-zerror <sunny.zerror@gmail.com>
2026-07-08 15:43:53 +0530

    values fix

--- src/components/home/Expertise.jsx
+++ src/components/home/Expertise.jsx
@@ -26,7 +26,8 @@ const Expertise = () => {
         })
         tl.to(".outer_circ", {
             scale: 1,
-            stagger: 0.1
+            stagger: 0.1,
+            duration:0.5,
         })
         tl.to(".inner_circ", {
             scale: 1,
@@ -47,29 +48,33 @@ const Expertise = () => {
 
         tl.to(".outer_circ", {
             rotate: 90,
-        }, "<+=0.85")
+        }, "<+=0.25")
         tl.to(".inner_circ", {
             scale: 0,
         }, "<")
         tl.to(".outer_center", {
             backgroundColor: "#86B039",
             duration: 0.2,
-        })
+         }, "<+=0.25")
         tl.to(".outer_center", {
             width: "14vw",
             height: "14vw",
+            duration: 0.2,
         }, "<+=0.5")
         tl.to(".outer_center", {
             width: "21vw",
             height: "21vw",
+            duration: 0.2,
         })
         tl.to(".outer_center", {
             width: "28vw",
             height: "28vw",
+            duration: 0.2,
         })
         tl.to(".outer_center", {
             width: "35vw",
             height: "35vw",
+            duration: 0.2,
         })
         tl.to(".outer_circ", {
             opacity: 0,
@@ -174,40 +179,40 @@ const Expertise = () => {
           <Button label={"Explore Our Expertise"} />
         </div>
       </div>
-      <div ref={containerRef} className=' relative h-[400vh] bg-white  w-full ' >
+      <div ref={containerRef} className=' relative h-[300vh] bg-white  w-full ' >
         <div className="container sticky! top-0 w-full h-screen!  overflow-hidden">
 
           <div className="w-full h-full max-sm:scale-150 center">
-            <div className=" last_circ_paren opacity-0 size-[35vw] aspect-square border border-[#00000050] absolute flex items-end justify-center bg-[#86B039] rounded-full">
+            <div className=" last_circ_paren opacity-0 size-[35vw] aspect-square border border-[#00000030] absolute flex items-end justify-center bg-[#86B039] rounded-full">
               <div className="pulse_ring_2 absolute w-full h-full rounded-full bg-[#86B039] opacity-80"></div>
-              <div className=" last_1 opacity-0 size-[35vw] aspect-square border border-[#00000050] absolute bg-white/28 rounded-full"></div>
-              <div className="last_2 opacity-0 size-[35vw] aspect-square border border-[#00000050] absolute rounded-full bg-white/40 "></div>
-              <div className="last_3 opacity-0 size-[35vw] aspect-square border border-[#00000050] absolute rounded-full bg-white/70 "></div>
+              <div className=" last_1 opacity-0 size-[35vw] aspect-square border border-[#00000030] absolute bg-white/28 rounded-full"></div>
+              <div className="last_2 opacity-0 size-[35vw] aspect-square border border-[#00000030] absolute rounded-full bg-white/40 "></div>
+              <div className="last_3 opacity-0 size-[35vw] aspect-square border border-[#00000030] absolute rounded-full bg-white/70 "></div>
             </div>
-            <div className=" outer_circ  scale-0 size-[35vw] aspect-square border border-[#00000050] absolute center rounded-full">
-              <div className=" inner_circ scale-0 absolute size-[1vw] aspect-square border border-[#00000050] bg-[#ffffff] rounded-full -translate-x-[4vw] translate-y-[-17vw]"></div>
-              <div className="inner_circ scale-0 absolute size-[4vw] aspect-square border border-[#00000050]  rounded-full bg-[#ffffff] grad_circle center -translate-x-[17.5vw]">
+            <div className=" outer_circ  scale-0 size-[35vw] aspect-square border border-[#00000030] absolute center rounded-full">
+              <div className=" inner_circ scale-0 absolute size-[1vw] aspect-square border border-[#00000030] bg-[#ffffff] rounded-full -translate-x-[4vw] translate-y-[-17vw]"></div>
+              <div className="inner_circ scale-0 absolute size-[4vw] aspect-square border border-[#00000030]  rounded-full bg-[#ffffff] grad_circle center -translate-x-[17.5vw]">
                 <div className="pulse_ring absolute w-full h-full rounded-full bg-[#ffffff] opacity-80"></div>
               </div>
-              <div className="inner_circ scale-0 absolute size-[4vw] aspect-square border border-[#00000050]  rounded-full bg-[#ffffff] grad_circle center translate-x-[8vw] translate-y-[15.5vw]">
+              <div className="inner_circ scale-0 absolute size-[4vw] aspect-square border border-[#00000030]  rounded-full bg-[#ffffff] grad_circle center translate-x-[8vw] translate-y-[15.5vw]">
                 <div className="pulse_ring absolute w-full h-full rounded-full bg-[#ffffff] opacity-80"></div>
               </div>
             </div>
-            <div className=" outer_circ  scale-0 size-[28vw] aspect-square border border-[#00000050] absolute center rounded-full">
-              <div className="inner_circ scale-0 absolute size-[2vw] aspect-square border border-[#00000050] bg-[#ffffff] rounded-full -translate-x-[10vw] translate-y-[10vw]"></div>
-              <div className="inner_circ scale-0 absolute size-[1.5vw] aspect-square border border-[#00000050] bg-[#ffffff] rounded-full translate-x-[5vw] translate-y-[-13vw]"></div>
+            <div className=" outer_circ  scale-0 size-[28vw] aspect-square border border-[#00000030] absolute center rounded-full">
+              <div className="inner_circ scale-0 absolute size-[2vw] aspect-square border border-[#00000030] bg-[#ffffff] rounded-full -translate-x-[10vw] translate-y-[10vw]"></div>
+              <div className="inner_circ scale-0 absolute size-[1.5vw] aspect-square border border-[#00000030] bg-[#ffffff] rounded-full translate-x-[5vw] translate-y-[-13vw]"></div>
             </div>
-            <div className=" outer_circ  scale-0 size-[21vw] aspect-square border border-[#00000050] absolute center rounded-full">
-              <div className=" inner_circ scale-0 absolute size-[2vw] aspect-square border border-[#00000050] rounded-full center translate-x-[9.5vw] bg-[#ffffff] grad_circle translate-y-[-4.5vw]">
+            <div className=" outer_circ  scale-0 size-[21vw] aspect-square border border-[#00000030] absolute center rounded-full">
+              <div className=" inner_circ scale-0 absolute size-[2vw] aspect-square border border-[#00000030] rounded-full center translate-x-[9.5vw] bg-[#ffffff] grad_circle translate-y-[-4.5vw]">
                 <div className="pulse_ring absolute w-full h-full rounded-full bg-[#ffffff] opacity-80"></div>
               </div>
-              <div className=" inner_circ scale-0 absolute size-[1vw] aspect-square border border-[#00000050] bg-[#ffffff] rounded-full translate-x-[1.5vw] translate-y-[10.3vw]"></div>
+              <div className=" inner_circ scale-0 absolute size-[1vw] aspect-square border border-[#00000030] bg-[#ffffff] rounded-full translate-x-[1.5vw] translate-y-[10.3vw]"></div>
             </div>
-            <div className=" outer_circ  scale-0 size-[14vw] aspect-square border border-[#00000050] absolute center rounded-full">
-              <div className="inner_circ scale-0 absolute size-[2vw] aspect-square border border-[#00000050] bg-[#ffffff] rounded-full -translate-x-[5vw] translate-y-[-4.9vw]"></div>
+            <div className=" outer_circ  scale-0 size-[14vw] aspect-square border border-[#00000030] absolute center rounded-full">
+              <div className="inner_circ scale-0 absolute size-[2vw] aspect-square border border-[#00000030] bg-[#ffffff] rounded-full -translate-x-[5vw] translate-y-[-4.9vw]"></div>
             </div>
-            <div className=" outer_circ outer_center scale-0 size-[7vw] aspect-square border border-[#00000050] absolute center rounded-full">
-              <div className="inner_circ scale-0 absolute size-[1vw] aspect-square border border-[#00000050] rounded-full center translate-y-[3.35vw] bg-[#ffffff] grad_circle translate-x-[0.5vw]">
+            <div className=" outer_circ outer_center scale-0 size-[7vw] aspect-square border border-[#00000030] absolute center rounded-full">
+              <div className="inner_circ scale-0 absolute size-[1vw] aspect-square border border-[#00000030] rounded-full center translate-y-[3.35vw] bg-[#ffffff] grad_circle translate-x-[0.5vw]">
                 <div className="pulse_ring absolute w-full h-full rounded-full bg-[#ffffff] opacity-80"></div>
               </div>
             </div>
